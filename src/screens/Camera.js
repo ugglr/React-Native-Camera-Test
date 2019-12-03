b9adec55bd69681968eeaf1243003da1bf1a5871
Carl-W <carl.igelstroem@tiket.com>
2019-12-03 09:03:57 +0800

    scanning works, and displays out to the screen

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -1,35 +1,72 @@
 import React, {Component} from 'react';
 import {View, Text, Alert, TouchableOpacity, Image} from 'react-native';
 import {RNCamera} from 'react-native-camera';
+import BarcodeMask from 'react-native-barcode-mask';
 import styled from 'styled-components/native';
+
+const CyberGreen = '#4caf50';
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: plum;
+  background-color: black;
+`;
+const ScanContainer = styled.View`
+  height: 150;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: black;
+  padding: 16px 16px;
+`;
+const Title = styled.Text`
+  font-size: 18;
+  color: white;
+`;
+const InfoText = styled.Text`
+  color: white;
+  font-size: 22;
 `;
 
 class CameraScreen extends Component {
   state = {
-    refresh: 0,
-    torchOn: false,
-    captureAudio: false,
+    scanInfo: '',
   };
-  refresh() {
-    this.setState({refresh: 0.00001});
-    this.setState({refresh: 0});
-  }
+
+  onBarCodeRead = e => {
+    const data = e.data;
+    this.setState({scanInfo: data});
+  };
 
   render() {
     return (
       <Container>
         {this.props.isFocused && (
           <RNCamera
-            style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0}}
             captureAudio={false}
-          />
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              bottom: 150,
+              left: 0,
+            }}
+            onBarCodeRead={this.onBarCodeRead}>
+            <BarcodeMask
+              edgeColor={CyberGreen}
+              animatedLineColor={CyberGreen}
+            />
+          </RNCamera>
         )}
+        <ScanContainer>
+          <Title>Scan INFO:</Title>
+          <InfoText>{this.state.scanInfo}</InfoText>
+        </ScanContainer>
       </Container>
     );
   }
